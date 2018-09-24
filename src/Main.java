cf23efbe8c3bb4022beab01aae35a59937dda183
jllys <jeanluc@gmail.com>
2018-09-24 10:42:33 +0200

    important
    gros c important

--- src/Main.java
+++ src/Main.java
@@ -31,6 +31,7 @@ public class Main {
 		//J'AI MODIFIER ! et là aussi ! '^'
 		
 		// cc c moi jluc
+		// vincent t un pd 
 		System.out.println(continent.getPays("France").getSaCapitale().getNom());
 		System.out.println(pays1.getLesVilles().get(0).getSonPays().getNom());
 	}
